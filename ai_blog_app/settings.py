ce4b1d240aca3e055a6f5dad6851cdff32876f13
Omar Fawzy <omarfawzy@Omars-MacBook-Air.local>
2025-08-26 16:40:19 +0300

    edit db

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'ai_blog_app.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL")),
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL")),
     "engine": "django.db.backends.postgresql"
 }
 
